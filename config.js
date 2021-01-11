0c83aa3ffb1ff8abe7bbd1aba053f3771fef371e
Teis Draiby <teis@teis.net>
2021-01-11 13:51:08 +0100

    Play stream using WowzaWebRTC library

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+	sdpURL: "wss://5feee5f959882.streamlock.net/webrtc-session.json",
+	applicationName: "live",
+	streamName: "fjellebroen.stream"
+};
---
+++ script.js
@@ -0,0 +1,19 @@
+import Config from './config.js';
+import WowzaWebRTCPlay from './lib/WowzaWebRTCPlay.js';
+
+
+window.wowzaWebRTCPlay = new WowzaWebRTCPlay();
+wowzaWebRTCPlay.on({
+	onError: (error) => {
+		console.log( JSON.stringify(error) )
+	},
+	onStateChanged: (state) => {
+		console.log (state.connectionState);
+	}
+});
+
+wowzaWebRTCPlay.set({ videoElementPlay: document.getElementById('video') });
+
+wowzaWebRTCPlay.set(Config);
+
+wowzaWebRTCPlay.play();
