ec7a0a8efed22b5f18097516a4294b5c97db6d35
Teis Draiby <teis@teis.net>
2021-01-30 18:13:21 +0100

    Test of Camera instantiation

--- CameraStreamingApi/Controllers/WeatherForecastController.cs
+++ CameraStreamingApi/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OnvifCamera;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +18,12 @@ namespace CameraStreamingApi.Controllers
 		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
+		private readonly ICamera camera;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		public WeatherForecastController(ILogger<WeatherForecastController> logger, ICamera camera)
 		{
 			_logger = logger;
+			this.camera = camera;
 		}
 
 		[HttpGet]
