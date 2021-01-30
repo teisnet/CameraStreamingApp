0a95b08e5d81285c3d75026b0b8b8b0bd282ac8c
Teis Draiby <teis@teis.net>
2021-01-30 18:16:39 +0100

    Inject logger into camera constructor

--- OnvifCamera/Camera.cs
+++ OnvifCamera/Camera.cs
@@ -1,8 +1,28 @@
-using System;
+﻿using System;
+using Microsoft.Extensions.Logging;
 
 namespace OnvifCamera
 {
 	public class Camera : ICamera
 	{
+		private readonly ILogger<Camera> logger;
+
+		public Camera()
+		{
+			Console.WriteLine("Default constructor");
+		}
+
+		// The dependency injection container will automatically use this constructor.
+		public Camera(ILogger<Camera> logger)
+		{
+			this.logger = logger;
+
+			logger.LogInformation("ILogger constructor");
+		}
+
+		public void Move()
+		{
+
+		}
 	}
 }
