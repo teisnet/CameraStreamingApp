6c506ac8e73f12a274ffa1d2cb6502a9a5315456
Teis Draiby <teis@teis.net>
2021-02-01 14:06:45 +0100

    Reload when appsettings are changed using IOptionsMonitor

--- OnvifCamera/Camera.cs
+++ OnvifCamera/Camera.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -7,15 +7,16 @@ namespace OnvifCamera
 	public class Camera : ICamera
 	{
 		private readonly ILogger<Camera> logger;
-		private readonly CameraConfig config;
+		private readonly IOptionsMonitor<CameraConfig> config;
+		public string Uri => config.CurrentValue.Uri;
 
 		public Camera() { }
 
 		// The dependency injection container will automatically use this constructor.
-		public Camera(IOptions<CameraConfig> options, ILogger<Camera> logger)
+		public Camera(IOptionsMonitor<CameraConfig> options, ILogger<Camera> logger)
 		{
 			this.logger = logger;
-			this.config = options.Value;
+			this.config = options;
 		}
 
 		public void Move()
