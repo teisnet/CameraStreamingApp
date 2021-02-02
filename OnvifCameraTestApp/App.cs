8da97664ef27bcfe216f6a9af584e892c31d472a
Teis Draiby <teis@teis.net>
2021-02-02 23:06:14 +0100

    First interaction with new Camera library: Snapshot

--- OnvifCameraTestApp/App.cs
+++ OnvifCameraTestApp/App.cs
@@ -9,14 +9,23 @@ namespace OnvifCameraTestApp
 	public class App
 	{
 		private readonly ILogger<App> logger;
+		private readonly ICamera camera;
 		private readonly CameraConfig cameraConfig;
 		private readonly AppSettings appSettings;
 
-		public App(IOptions<AppSettings> appSettings, ILogger<App> logger, IOptions<CameraConfig> cameraConfig)
+		public App(IOptions<AppSettings> appSettings, ILogger<App> logger, IOptions<CameraConfig> cameraConfig, ICamera camera)
 		{
 			this.logger = logger;
+			this.camera = camera;
 			this.cameraConfig = cameraConfig.Value;
 			this.appSettings = appSettings.Value;
+
+			camera.StatusChanged += Camera_StatusChanged;
+		}
+
+		private void Camera_StatusChanged(object sender, EventArgs e)
+		{
+			logger.LogInformation("Camera status changed");
 		}
 
 		public async Task Run(string[] args)
@@ -26,6 +35,13 @@ namespace OnvifCameraTestApp
 			logger.LogInformation($"App title: {appSettings.Title}");
 			logger.LogInformation($"Camera name: {cameraConfig.Name}");
 			logger.LogInformation($"Camera uri: {cameraConfig.Uri}");
+
+			TestCamera();
+		}
+
+		private void TestCamera()
+		{
+			camera.Enable();
 		}
 	}
 }
