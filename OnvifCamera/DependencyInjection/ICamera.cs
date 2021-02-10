76ba0be643237857ef4ee702542ac48c48d5a225
Teis Draiby <teis@teis.net>
2021-02-10 18:34:24 +0100

    Cleanup ICamera interface to bare minimum

--- OnvifCamera/DependencyInjection/ICamera.cs
+++ OnvifCamera/DependencyInjection/ICamera.cs
@@ -1,25 +1,17 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
 namespace OnvifCamera
 {
 	public interface ICamera
 	{
-		dynamic ActiveSource { get; set; }
-		dynamic Capabilities { get; set; }
-		dynamic DefaultProfile { get; set; }
 		string Name { get; }
-		dynamic Profiles { get; set; }
-		string Uri { get; }
-		dynamic VideoSources { get; set; }
 
-		event EventHandler Moving;
 		event EventHandler StatusChanged;
 
-		Task<bool> AbsoluteMove(PtzValue position);
 		void Disable();
 		Task Enable();
 		Task<string> GetSnapshot();
-		Task MoveTo(CameraPosition position);
+		Task<bool> AbsoluteMove(PtzValue position);
 	}
 }
