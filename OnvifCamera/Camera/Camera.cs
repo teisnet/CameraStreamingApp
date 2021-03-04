1b25f357dc14a14ac20d43dca0daf72e7aba98ee
Teis Draiby <teis@teis.net>
2021-03-04 19:04:35 +0100

    Get rid of Camera base class

--- OnvifCamera/Camera/Camera.cs
+++ OnvifCamera/Camera/Camera.cs
@@ -7,13 +7,46 @@ using Newtonsoft.Json.Linq;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using System.IO;
+using System.Net;
 
 namespace OnvifCamera
 {
+	/*
+	 * https://github.com/aspnet/JavaScriptServices/tree/master/src/Microsoft.AspNetCore.NodeServices
+	 */
+
 	public enum MoveCommand { Stop, Left, Right, Up, Down, ZoomOut, ZoomIn }
 
-	public class Camera : CameraBase, ICamera
+	public class Camera : ICamera
 	{
+		public string Name => config.Name;
+
+		// iSInitialized should never be set back to false after setting it to true.
+		protected bool isInitialized = false;
+
+		public bool IsEnabled { get; protected set; }
+		public bool IsOnline { get; protected set; }
+		public CameraStatus Status { get => new(IsEnabled, IsOnline); }
+		public PtzValue Position { get => position; }
+
+		protected bool pendingStatus;
+		protected bool pendingConnect;
+		protected bool isMoving;
+		protected bool isMovingToTarget;
+
+		// Position , moveTarget and previousPosition in degrees (not internal camera values)
+		// TODO: In newing necesary?
+		protected PtzValue position = new();
+		protected PtzValue previousPosition = new();
+		protected PtzValue moveTarget = new();
+
+		private INodeServices nodeService;
+		private string nodeFilename = "./Node/node-camera.js";
+
+		protected readonly ILogger<Camera> logger;
+		protected CameraConfig config;
+		private int configHash;
+
 		// Timers
 		// Reconnect time
 		private Timer heartbeatTimer = new Timer(7000);
@@ -38,12 +71,33 @@ namespace OnvifCamera
 		public event EventHandler StatusChanged;
 
 		public Camera(IOptionsMonitor<CameraConfig> config, ILogger<Camera> logger, INodeServices nodeServices)
-			: base(config, logger, nodeServices)
 		{
+			this.logger = logger;
+			this.config = config.CurrentValue;
+			this.configHash = this.config.GetHashCode();
+			
 			heartbeatTimer.Elapsed += (sender, e) => Connect();
 			statusTimer.Elapsed += (sender, e) => UpdateStatus();
 
-			// TODO: Cannot automatically enable on initialization since it cannot be awaited inside a constructor.
+			// This event is fired when the camera settings in appsetting.json are changed.
+			config.OnChange(config =>
+			{
+				// For some reason OnChange is fired twice per change. Don't act if the config properties haven't changed.
+				// See https://github.com/dotnet/aspnetcore/issues/2542
+				var newConfigHash = config.GetHashCode();
+
+				if (newConfigHash != configHash)
+				{
+					this.config = config;
+					configHash = newConfigHash;
+					logger.LogInformation($"[{Name}]: The camera configuration has been updated.");
+
+					OnConfigChange();
+				}
+			});
+
+			this.nodeService = nodeServices;
+
 			// if (this.config.Enabled)
 			// { await Enable(); }
 			// else { logger.LogWarning($"{Name}]: disabled"); }
@@ -349,6 +403,70 @@ namespace OnvifCamera
 		{
 			// Enabled
 			StatusChanged?.Invoke(this, EventArgs.Empty);
+		}
+
+		protected void OnConfigChange()
+		{
+		}
+
+		protected async Task<T> Call<T>(string function, params object[] args)
+		{
+			try
+			{
+				return await nodeService.InvokeExportAsync<T>(nodeFilename, function, args);
+			}
+			catch (Exception e)
+			{
+				logger.LogError(e, $"[{Name}]: Error when calling Node function '{function}()': " + e.InnerException?.Message ?? e.Message);
+				return default;
+			}
+		}
+
+		private async Task<Uri> GetSnapshotUri()
+		{
+			string uriString = await Call<string>("getSnapshot");
+
+			UriBuilder snapshotUri = new UriBuilder(uriString);
+
+			// Replace host and port values as they might be LAN specific values.
+			snapshotUri.Host = config.Uri;
+			snapshotUri.Port = config.WebPort;
+			snapshotUri.UserName = config.Username;
+			snapshotUri.Password = config.Password;
+
+			return snapshotUri.Uri;
+		}
+
+		/// <summary>
+		/// Downloads a snapshot from the camera.
+		/// </summary>
+		/// <returns>Filename of the downloaded snapshot image.</returns>
+		public async Task<string> GetSnapshot()
+		{
+			if (!isInitialized)
+			{
+				throw new InvalidOperationException($"Cannot get snapshot when camera \"{Name}\" is not yet initialized.");
+			}
+
+			// Formatting DateTime: https://stackoverflow.com/questions/7898392/append-timestamp-to-a-file-name
+
+			string filename = $"snapshot_{config.Slug}_{DateTime.Now:yyyy-MM-ddTHH-mm-ss}.jpg";
+			Uri uri = await GetSnapshotUri();
+
+			using (var client = new WebClient())
+			{
+				try
+				{
+					await client.DownloadFileTaskAsync(uri, filename);
+				}
+				catch (Exception e)
+				{
+					logger.LogError($"[{Name}]: Could not download snapshot from {uri.Scheme}://{uri.Host}:{uri.Port}{uri.PathAndQuery} (credentials are omitted here)");
+					return default;
+				}
+			}
+
+			return filename;
 		}
 	}
 }
